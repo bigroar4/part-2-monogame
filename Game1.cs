a08006c1400e9d557a76b7474ff1a2c34b231b97
Isaac Rohrer <RohrIsaa298@GoTVDSB.ca>
2022-11-22 09:40:00 -0500

    finnished work of someone with expertiese

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,14 @@ namespace part_2_monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D circletexture;
+        Rectangle circlerect, circle2Rect;
+        Texture2D squaretexture;
+        Rectangle rectanglerect;
+
+        
+        
+        SpriteFont titleFont;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,12 +28,20 @@ namespace part_2_monogame
         {
             // TODO: Add your initialization logic here
 
+            rectanglerect = new Rectangle(140, 400, 250, 50);
+            circlerect = new Rectangle(350, 200, 100, 100);
+            circle2Rect = new Rectangle(150, 200, 100, 100);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            squaretexture = Content.Load<Texture2D>("rectangle");
+            circletexture = Content.Load<Texture2D>("circle");
+            titleFont = Content.Load<SpriteFont>("Font");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -45,6 +61,15 @@ namespace part_2_monogame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(circletexture,circlerect,Color.Magenta);
+            _spriteBatch.Draw(squaretexture,rectanglerect, Color.Crimson);
+            _spriteBatch.Draw(circletexture, circle2Rect, Color.Red);
+            _spriteBatch.DrawString(titleFont, "Mr.Rohrer" , new Vector2(100,100), Color.Purple);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
